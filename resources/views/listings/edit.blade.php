292560bf071cfd068b51d281b51e4b76c4a5a355
Darren Cal <darrencal64@gmail.com>
2022-08-10 21:47:05 -0600

    Added the ability to edit and delete listings

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -5,20 +5,21 @@
         >
             <header class="text-center">
                 <h2 class="text-2xl font-bold uppercase mb-1">
-                    Create a Gig
+                    Edit Gig
                 </h2>
-                <p class="mb-4">Post a gig to find a developer</p>
+                <p class="mb-4">Edit: {{ $listing->title }}</p>
             </header>
 
-            <form method="POST" action="/listings" enctype="multipart/form-data">
+            <form method="POST" action="/listings/{{ $listing->id }}" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="mb-6">
                     <label for="company" class="inline-block text-lg mb-2">Company Name</label>
                     <input
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="company"
-                        value="{{ old('company') }}"
+                        value="{{ $listing->company }}"
                     />
 
                     @error('company')
@@ -33,7 +34,7 @@
                         class="border border-gray-200 rounded p-2 w-full"
                         name="title"
                         placeholder="Example: Senior Laravel Developer"
-                        value="{{ old('title') }}"
+                        value="{{ $listing->title }}"
                     />
 
                     @error('title')
@@ -48,7 +49,7 @@
                         class="border border-gray-200 rounded p-2 w-full"
                         name="location"
                         placeholder="Example: Remote, Boston MA, etc"
-                        value="{{ old('location') }}"
+                        value="{{ $listing->location }}"
                     />
 
                     @error('location')
@@ -62,7 +63,7 @@
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="email"
-                        value="{{ old('email') }}"
+                        value="{{ $listing->email }}"
                     />
 
                     @error('email')
@@ -76,7 +77,7 @@
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="website"
-                        value="{{ old('website') }}"
+                        value="{{ $listing->website }}"
                     />
 
                     @error('website')
@@ -91,7 +92,7 @@
                         class="border border-gray-200 rounded p-2 w-full"
                         name="tags"
                         placeholder="Example: Laravel, Backend, Postgres, etc"
-                        value="{{ old('tags') }}"
+                        value="{{ $listing->tags }}"
                     />
 
                     @error('tags')
@@ -107,6 +108,12 @@
                         name="logo"
                     />
 
+                    <img
+                        class="w-48 mr-6 mb-6"
+                        src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('images/no-image.png') }}"
+                        alt=""
+                    />
+
                     @error('logo')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
@@ -119,7 +126,7 @@
                         name="description"
                         rows="10"
                         placeholder="Include tasks, requirements, salary, etc"
-                    >{{ old('description') }}</textarea>
+                    >{{ $listing->description }}</textarea>
 
                     @error('description')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -127,7 +134,7 @@
                 </div>
 
                 <div class="mb-6">
-                    <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">Create Gig</button>
+                    <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">Update Gig</button>
 
                     <a href="/" class="text-black ml-4">Back</a>
                 </div>
